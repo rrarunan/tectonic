d1fb72c38a4e76f9aed336a9327860a0cb0088ff
Tony Holdstock-Brown <tonyhb@gmail.com>
2016-09-29 20:58:30 -0700

    Add Status promise tests

--- packages/tectonic/test/specs/status/status.js
+++ packages/tectonic/test/specs/status/status.js
@@ -5,7 +5,9 @@ import * as consts from '/src/consts.js';
 
 describe('status', () => {
   let status;
-  let promise = new Promise((resolve, reject) => resolve('ya'));
+  let promise = new Promise((resolve, reject) => {
+    resolve('ya');
+  });
 
   describe('constructor', () => {
     it('has a default status of PENDING if undefined', () => {
@@ -51,7 +53,51 @@ describe('status', () => {
   });
 
   describe('promises', () => {
-    it('chains promises together', () => {
+    describe('success', () => {
+      it('chains .then promises together when resolved', (done) => {
+        let first, second;
+
+        status = new Status({ promise });
+        status.then(() => { first = 1 }).then(() => { second = 2 });
+
+        // Assert that the .then functions are called correctly.
+        // Note that promises call their functions in a separate tick, therefore
+        // this will not work immediately and we need to check after a timeout.
+        // Mocha waits until done() is called to end the test.
+        window.setTimeout(() => {
+          assert.equal(first, 1);
+          assert.equal(second, 2);
+          done();
+        }, 5);
+      });
+    });
+
+    describe('failure', () => {
+      let rejected = new Promise((resolve, reject) => {
+        reject('no');
+      });
+
+      it('calls .catch on failure', (done) => {
+        let first;
+        status = new Status({ promise: rejected });
+        status.catch(() => { first = 1 });
+
+        window.setTimeout(() => {
+          assert.equal(first, 1);
+          done();
+        }, 5);
+      });
+
+      it('calls .then on failure', (done) => {
+        let first;
+        status = new Status({ promise: rejected });
+        status.then(() => {}, () => { first = 1 });
+
+        window.setTimeout(() => {
+          assert.equal(first, 1);
+          done();
+        }, 5);
+      });
     });
   });
 
